c631c39b9678c9704a969c349d4ea62a7b241843
U0Nkis <trufanov-e95@mail.ru>
2024-09-25 14:46:12 +0300

    init

---
+++ src/main/java/org/example/sanatoriumcomplex/SanatoriumComplexApplication.java
@@ -0,0 +1,13 @@
+package org.example.sanatoriumcomplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanatoriumComplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanatoriumComplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sanatoriumcomplex/SanatoriumComplexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sanatoriumcomplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanatoriumComplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
